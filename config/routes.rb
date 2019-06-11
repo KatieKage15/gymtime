b6235e0e34786494dd7714a7fb2809dce063d2fa
Katie Wilday <wildayk@gmail.com>
2019-06-11 14:30:14 -0400

    views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :instructors
-  resources :clients
+  resources :clients, only: [:create, :show, :new]
   resources :trainings
 
   get '/signin', to: 'sessions#new'
