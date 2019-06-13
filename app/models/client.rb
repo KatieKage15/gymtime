20665a646386fb56bbf935baa709f0487261aafe
Katie Wilday <wildayk@gmail.com>
2019-06-13 08:28:47 -0400

    find_create omniauth method

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,13 @@
 class Client < ApplicationRecord
   has_many :trainings
   has_many :instructors, through: :trainings
+
+  def self.find_or_create_by_omniauth(auth_hash)
+    oauth_username = auth_hash[:username]
+    if client = Client.find_by(:username => oauth_username)
+      return client
+    else
+      client = Client.create(:username => oauth_username, :password => SecureRandom.hex)
+    end
+  end
 end
