b2715cbfc39f6eaab063b6a8976e4b7e80e6a2dd
Katie Wilday <wildayk@gmail.com>
2019-06-16 09:28:45 -0400

    updating models

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -4,4 +4,7 @@ class Instructor < ApplicationRecord
 
   scope :certified, -> { where(certified: true) }
   scope :instructor, -> (name) { where(instructor_name: name) }
+
+  validates :name, presence: true
+  validates :email, uniqueness: true
 end
--- app/models/training.rb
+++ app/models/training.rb
@@ -1,4 +1,10 @@
 class Training < ApplicationRecord
   belongs_to :client
-  belongs_to :instructor 
+  belongs_to :instructor
+
+  validates :name, presence: true
+
+  def request_training 
+    "Thanks for requesting a training session with #{self.instructor.name}! They will see you at #{self.instructor.gym} soon!"
+  end
 end
