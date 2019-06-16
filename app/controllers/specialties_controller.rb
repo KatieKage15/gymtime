ba666bf779d5212dd528f8f43a23f46f08ab13ea
Katie Wilday <wildayk@gmail.com>
2019-06-16 09:06:14 -0400

    added specialty controller

---
+++ app/controllers/specialties_controller.rb
@@ -0,0 +1,17 @@
+class SpecialtyController < ApplicationController
+
+  def index
+    @instructor = Instructor.all
+     if params[:client_id]
+       @specialty = Client.find(params[:client_id]).specialty
+     else
+       @specialty = Specialty.all
+    end
+   end
+
+  def create
+    specialty = Specialty.create(client_id: current_user.id, instructor_id: params[:instructor_id])
+    redirect_to root_path
+  end
+  
+end
